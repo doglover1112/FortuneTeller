e025fc39735ed77b25de37a6d9530ceb832e2290
Apple <apple@Apples-MacBook-Pro.local>
2019-06-18 11:04:16 -0600

    FortuneTeller

---
+++ index.js
@@ -0,0 +1,16 @@
+var present = document.querySelector('.present');
+var bottle = document.querySelector('.bottle');
+var tent = document.querySelector('.tent');
+var message = document.querySelector('.message');
+
+present.addEventListener('click', presentFunction);
+bottle.addEventListener('click', bottleFunction);
+tent.addEventListener('click', tentFunction);
+
+presentMessages = ["", ""];
+bottleMessages = ["", ""];
+tentMessages = ["", ""];
+
+function presentFunction(){
+  
+}
